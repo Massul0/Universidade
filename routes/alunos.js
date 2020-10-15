b09b7ae8894209fa7c454e5784fc5e2511a09fec
Massulo <ramon.massulo@yahoo.com.br>
2020-10-14 23:53:22 -0300

    JWT Token

--- routes/alunos.js
+++ routes/alunos.js
@@ -2,6 +2,8 @@ const express = require('express');
 const { render } = require('../app');
 const router = express.Router();
 const mysql = require('../mysql').pool;
+const jwt = require('jsonwebtoken');
+const login = require('../middleware/login');
 
 
 
@@ -26,26 +28,38 @@ router.post('/login', (req, res, next)=>{
                         mensagem: 'Falha na autenticação'
                     })
                 }
-                return res.status(200).send({
-                    status_code: 200,
-                    auth: true, 
-                    mensagem: 'Autenticado com sucesso'
-                })
+                if(resultado){
+                    const token = jwt.sign({
+                        cod: resultado[0].id
+                    }, 
+                    process.env.JWT_KEY, 
+                    {
+                        expiresIn: "50s"
+                    });
+                    return res.status(200).send({
+                        status_code: 200,
+                        auth: true,
+                        cod: resultado[0].id, 
+                        mensagem: 'Autenticado com sucesso',
+                        token: token
+                    })
+
+                }
             }
         )
     })
 })
 
 // Retorna os dados de um aluno
-router.get('/:cpf', (req, res, next)=>{
+router.get('/consultaAluno/:id', login, (req, res, next)=>{
     mysql.getConnection((error, conn) => {
         if(error) {return res.status(500).send({ error: error}) }
         conn.query(
-            'SELECT * FROM pessoa WHERE cpf = ?;',
-            [req.params.cpf],
+            "SELECT t.sala AS Sala, d.nome AS Disciplina, a.matricula AS Matricula, CONCAT(p.pnome, ' ', p.unome) AS Nome, t.primeira_nota AS 1_Nota, t.segunda_nota AS 2_Nota, t.faltas AS Faltas FROM turma AS t INNER JOIN disciplina AS d ON t.fk_turma_disciplina = d.id INNER JOIN aluno AS a ON t.fk_turma_aluno = a.id INNER JOIN pessoa AS p ON a.id = p.id INNER JOIN pessoa AS pr ON pr.id = p.id WHERE p.id = ?",
+            [req.params.id],
             (error, resultado, fields) => {
                 if (error) { return res.status(500).send({ error: error}) }
-                return res.status(200).send({response: resultado})
+                return res.status(200).send({status_code: 200, lista: resultado})
             }
         )
 
